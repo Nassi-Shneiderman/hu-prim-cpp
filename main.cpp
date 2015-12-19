8d3edbaeef46fe7edd215049a4f8e50f531f60e1
Shneiderman <githubaccount4school@web.de>
2015-12-19 11:01:50 +0100

    upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
+#include <cstdlib>
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+void clean() {
+    system("cls");
+}
+
+void prim(){
+    clean();
+    int inputNum;
+    cout << "Zahl: ";
+    cin >> inputNum;
+};
+
+void mirp(){
+    clean();
+    int inputNum;
+    cout << "Zahl: ";
+    cin >> inputNum;
+};
+
+void menu() {
+    char auswahl;
+    cout << "---|Menu|---" << endl;
+    cout << "(1) Primzahl" << endl;
+    cout << "(2) Mirpzahl" << endl;
+    cout << "(3) Ende" << endl;
+    cout << "------------" << endl;
+    cout << "Auswahl: "; cin >> auswahl;
+
+    switch(auswahl){
+        case '1':
+        case 'p':
+        case 'P':
+            prim();
+        break;
+
+        case '2':
+        case 'm':
+        case 'M':
+            mirp();
+        break;
+
+        case '3':
+        case 'e':
+        case 'E':
+            exit(EXIT_SUCCESS);
+        break;
+    }
+}
+
+int main() {
+    menu();
 }
